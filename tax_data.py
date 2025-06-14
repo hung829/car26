72f37df1d8fca59950c621bb1adfa770910eb938
hung829 <minhhunggg1706@gmail.com>
2025-06-14 13:11:41 +0700

    Create tax_data.py

---
+++ tax_data.py
@@ -0,0 +1,12 @@
+tax_records = {
+    "123456789": {
+        "Tên công ty": "Công ty TNHH ABC",
+        "Địa chỉ": "123 Lê Lợi, TP.HCM",
+        "Ngành nghề": "Thương mại"
+    },
+    "987654321": {
+        "Tên công ty": "Công ty Cổ phần XYZ",
+        "Địa chỉ": "456 Trần Hưng Đạo, Hà Nội",
+        "Ngành nghề": "Sản xuất"
+    }
+}
